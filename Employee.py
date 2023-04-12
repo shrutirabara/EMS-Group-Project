3c3197f7971d54261e0df97e8c8ad04805fe3503
newdevon <stvchan999@gmail.com>
2023-04-12 12:36:15 -0400

    added class methods for Employee

--- Employee.py
+++ Employee.py
@@ -1,3 +1,40 @@
+    # Getter Methods
 
+    def getFirstName(self):
+        return self.__firstName
+    def getLastName(self):
+        return self.__lastName
+    def getEmployeeId(self):
+        return self.__employeeId
+    def getDateOfEmployment(self):
+        return self.__dateOfEmployment
+    def getSalary(self):
+        return self.__salary
+    def getDepartment(self):
+        return self.__department
+
+    # Setter Methods
+
+    def setFirstName(self, fnInput):
+        self.__firstName = fnInput
+        print(f"First Name set to '{fnInput}'")
+    def setLastName(self, lnInput):
+        self.__lastName = lnInput
+    def setEmployeeId(self, idInput):
+        self.__employeeId = idInput
+    def setDateOfEmployment(self, doeInput):
+        self.__dateOfEmployment = doeInput
+    def setSalary(self, salaryInput):
+        self.__salary = salaryInput
+    def setDepartment(self, dInput):
+        self.__department = dInput
+
+
+Steven = Employee("steven", "chan", "23", "April 13th, 2023", "75000", "Web Development")
+
+print(Steven.getFirstName(), Steven.getLastName(), Steven.getEmployeeId, Steven.getDateOfEmployment, Steven.getSalary, Steven.getDepartment)
+
+Sadie = Employee("sadie", "kc", "12" "June 5th, 2022", "65000", "Data Administrator")
+print(Sadie.getFirstName, Steven.getLastName(), Sadie.getEmployeeId, Sadie.getDateOfEmployment, Sadie.getSalary, Sadie.getDepartment)
 
     
